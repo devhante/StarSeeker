1c3fb5b858d4ec0a527d0020d779065c1d2a5675
SCOUTgit <k01095232802@gmail.com>
2018-12-24 14:11:16 +0900

    [Add] Star,  Block 움직이는 기능 추가

---
+++ Assets/Scripts/PlayScene/Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine("MoveBlock");
+    }
+
+    private IEnumerator MoveBlock()
+    {
+        while (transform.position.y < 6)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayScene/Star.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public float jumpHeight = 1;
+
+    private Rigidbody2D rigidbody2D;
+    private bool jumping = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        StartCoroutine("MoveStar");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       
+    }
+
+    private IEnumerator MoveStar()
+    {
+        while (gameObject.activeSelf)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x < -0.05)
+                {
+                    transform.Translate(Vector2.left * Time.deltaTime);
+                }
+                else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x > 0.05)
+                {
+                    transform.Translate(Vector2.right * Time.deltaTime);
+                }
+            }
+            yield return null;
+        }
+    }
+
+    private void JumpStar()
+    {
+        jumping = true;
+        rigidbody2D.AddForce(Vector2.up * jumpHeight * 100);
+        jumping = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Block" && !jumping)
+            JumpStar();
+    }
+}
